f8601c9074faef591cf33291ee9929b42ca2c5dd
Balbina <balbinasantana@gmail.com>
2016-07-07 12:10:55 -0500

    New version

--- lib/administrate/field/enum.rb
+++ lib/administrate/field/enum.rb
@@ -4,7 +4,7 @@ require 'rails'
 module Administrate
   module Field
     class Enum < Administrate::Field::Base
-      VERSION = '0.0.1'
+      VERSION = '0.0.2'
 
       def to_s
         data.humanize
