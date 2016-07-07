af409bfd144dc441b0e0c5fe874cfc4bb20c043b
Balbina <balbinasantana@gmail.com>
2016-07-07 11:56:01 -0500

    Humanize hiccup

--- lib/administrate/field/enum.rb
+++ lib/administrate/field/enum.rb
@@ -6,6 +6,10 @@ module Administrate
     class Enum < Administrate::Field::Base
       VERSION = '0.0.1'
 
+      def to_s
+        data.humanize
+      end
+
       class Engine < ::Rails::Engine
       end
     end
